6689308cfb6d816230b461406d70e0317a1b34c6
Ali Karakoc <ali.karakoc.eee@gmail.com>
2018-03-26 11:58:17 +0300

    Add files via upload

--- lexicographic.cpp
+++ lexicographic.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Homework4.cpp
-// Author      : Ali KarakoÃ§
+// Author      : Ali Karakoç
 // Version     : 1.0
 // Copyright   : Your copyright notice
 // Description : Dynamic size array lexography, Ansi-style
@@ -16,17 +16,18 @@ int main() {
 int seqnum;
 int size;
 
-cout << "Enter the sequence number as you wish!:"<<endl;
+cout << "Enter the sequence number you wish!:"<<endl;
 
 cin >> seqnum ;
 
-int** arraymatrix = new int*[seqnum];
-
+int *arraymatrix[seqnum];
+int sizes[seqnum];
 
 for(int i = 0; i < seqnum; i++)
 {
-	 cout << "Enter the size of the sequence no-"<< i <<":"<<endl;
+	cout << "Enter the size of the sequence no-"<< i <<":"<<endl;
      cin >> size;
+     sizes[i]=size;
      arraymatrix[i] = new int[size];
 	 cout << "Enter the sequence elements-"<< i <<":"<<endl;
      for(int j = 0; j < size; j++){
@@ -34,72 +35,77 @@ for(int i = 0; i < seqnum; i++)
      }
      cout << "sequence-["<< i <<"]=[";
      for(int j = 0; j < size; j++){
-    	 if(j==size-1){
+    	if(j==size-1){
        	 cout<< "" << arraymatrix[i][j];
-    	 }
-    	 else{
+    	}
+    	else{
        	 cout<< "" << arraymatrix[i][j]<<",";
-    	 }
+    	}
      }
      cout <<"]"<< endl;
 
 }
-cout << arraymatrix[0] << endl;
 
-for(int i = 0; i < seqnum-1; ++i)
+cout << "The array set that you entered:" << endl;
 
-      for( int k = i+1; k < seqnum; ++k)
+for (int i = 0; i < seqnum; ++i)
+{
 
-      {
-
-           int m=0;
-
-
-
-         if(arraymatrix[i][m] > arraymatrix[k][m])
-
-         {
-
-           int * temp = arraymatrix[i];
-
-           arraymatrix[i] = arraymatrix[k];
-
-           arraymatrix[k] = temp;
-
-         }
+    for (int j = 0; j < sizes[i]; j++)
+    {
+        cout << arraymatrix[i][j] << ' ';
+    }
+    cout << endl;
+}
 
 
+int maxsortsize=seqnum*(seqnum-1)/2+1;
 
-        if(arraymatrix[i][m] == arraymatrix[k][m]){
+for(int k=0; k<=maxsortsize; k++){
+        
+      for(int i=0; i<seqnum-1 ; i++){
+        int m=0;
 
-          while(arraymatrix[i][m] == arraymatrix[k][m]){
+		if(arraymatrix[i][m] > arraymatrix[i+1][m]){
 
-          m=m+1;
+			     int  *temp=arraymatrix[i];
+			     arraymatrix[i]=arraymatrix[i+1];
+			     arraymatrix[i+1]=temp;
+                 int tempSize;
+				 tempSize=sizes[i];
+				 sizes[i]=sizes[i+1];
+				 sizes[i+1]=tempSize;
+		}
+		if(arraymatrix[i][m] == arraymatrix[i+1][m]){
+                while(arraymatrix[i][m] == arraymatrix[i+1][m]){
+                m=m+1;			
+                }
+ 	            if(arraymatrix[i][m] > arraymatrix[i+1][m]){
+                 int  *temp=arraymatrix[i];
+			     arraymatrix[i]=arraymatrix[i+1];
+			     arraymatrix[i+1]=temp;
+                 int tempSize;
+				 tempSize=sizes[i];
+				 sizes[i]=sizes[i+1];
+				 sizes[i+1]=tempSize;
+				 m=0;
+               }
+		}
+		
+     }
 
-           }
+}
+cout << "The sorted array set is:" << endl;
 
-           if(arraymatrix[i][m] > arraymatrix[k][m])
+   for (int i = 0; i < seqnum; ++i)
+   {
 
-           {
-
-             int temp = arraymatrix[i];
-
-             arraymatrix[i] = arraymatrix[k];
-
-             arraymatrix[k] = temp;
-
-           }
-
-
-
-      }
-
+       for (int j = 0; j < sizes[i]; j++)
+       {
+           cout << arraymatrix[i][j] << ' ';
+       }
+       cout << endl;
    }
-
-
-
-
-
+   
 return 0;
-
 }
